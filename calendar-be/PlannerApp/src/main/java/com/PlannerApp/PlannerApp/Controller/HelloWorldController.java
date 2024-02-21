f906f53fa1eb27adb3f08b0238f9115ba1a2ed2a
Lukrow <lukroza1@gmail.com>
2024-02-22 01:10:35 +0200

    Initialized A spring boot application

---
+++ calendar-be/PlannerApp/src/main/java/com/PlannerApp/PlannerApp/Controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.PlannerApp.PlannerApp.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String getHelloMessage(){
+        return "Hello world";
+    }
+
+}
