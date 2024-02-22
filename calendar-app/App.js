a14b64d38f81abbea7b92fe2bd49a7b5cbf47506
Vilius Tamašauskas <viliustam36@gmail.com>
2024-02-22 12:29:13 +0200

    Added Header
    Implemented Header component to the app

--- calendar-app/App.js
+++ calendar-app/App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Header from './Components/Header'; // Assume your file is named Header.js
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Header />
+      </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, // Ensures the View fills the entire screen
+    backgroundColor: '#2F3855', // Sets the background color of the entire container
   },
 });
+
+export default App;
