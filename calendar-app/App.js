3ffe2e8da7a7b5102fb906f659a6459ee4a736f2
Lukrow <lukroza1@gmail.com>
2024-03-08 20:29:24 +0200

    Added refreshes after register

--- calendar-app/App.js
+++ calendar-app/App.js
@@ -3,12 +3,26 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { View, StyleSheet } from 'react-native';
 import { GlobalColor, GlobalSecondaryColor, GlobalFont } from './Styles';
 import RegistrationScreen from './Screens/RegistrationScreen';
+import { useState } from 'react';
+import Footer from './Components/Footer';
 
 
 const App = () => {
+  const [refreshKey, setRefreshKey] = useState(0);
+  const [isRegistered, setIsRegistered] = useState(false);
+  
+  const handleRefresh = () => {
+    setRefreshKey(oldKey => oldKey + 1);
+    setIsRegistered(true);
+  };
+  
   return (
     <SafeAreaProvider>
-      <RegistrationScreen />
+      {isRegistered ? (
+        <Footer /> // Render Footer if isRegistered is true
+      ) : (
+        <RegistrationScreen key={refreshKey} onRefresh={handleRefresh} /> // Render RegistrationScreen if isRegistered is false
+      )}
     </SafeAreaProvider>
   );
 };
--- calendar-app/Screens/RegistrationScreen.js
+++ calendar-app/Screens/RegistrationScreen.js
@@ -4,14 +4,18 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import TextInputBar from '../Components/TextInputBar';
 import ButtonComp from '../Components/ButtonComp';
 import { createUserApi } from '../Components/API/Users/UserRegister';
+import { storeUserInfo } from '../Components/Storage/userDataStorage';
 
-const RegistrationScreen = () => {
+const RegistrationScreen = (props) => {
   const [username, setUsername] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleRegistration = async () => {
     try {
-      await createUserApi({ username });
+      await createUserApi({ username }).then(async (response) => {
+        await storeUserInfo(response, false, true, "0");
+        props.onRefresh();
+      });
     } catch (error) {
       setErrorMessage(error.message);
     }
