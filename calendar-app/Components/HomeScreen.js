6138cb1bea67cb5240e17acf1b1d7d48259333d5
Oshernee <ignas.pau@gmail.com>
2024-03-08 19:21:25 +0200

    Remade user information storage with expo SecureStore

--- calendar-app/Components/HomeScreen.js
+++ calendar-app/Components/HomeScreen.js
@@ -1,48 +1,21 @@
-import React, {useState} from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import { storeUserInfo, getUserId, getInGroupStatus } from './Storage/userDataStorage'
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Header from '../Components/Header';
+import Calendar from '../Components/Calendar';
 
 const HomeScreen = () => {
-  const [userId, setUserId] = useState('');
-  const [inGroup, setInGroup] = useState(false);
-
-  const randomizeAndStoreUserInfo = async () => {
-    const randomUserId = Math.floor(Math.random() * 10000).toString();
-    const randomInGroup = Math.random() < 0.5;
-    await storeUserInfo(randomUserId, randomInGroup);
-    setUserId(randomUserId);
-    setInGroup(randomInGroup);
-  };
-
-  const displayStoredUserInfo = async () => {
-    const storedUserId = await getUserId();
-    const storedInGroupStatus = await getInGroupStatus();
-    console.log(`Stored User ID: ${storedUserId}, In Group: ${storedInGroupStatus}`);
-  };
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>User ID: {userId}</Text>
-      <Text>In Group: {inGroup ? 'Yes' : 'No'}</Text>
-      <Button
-        title="Randomize and Store User Info"
-        onPress={randomizeAndStoreUserInfo}
-      />
-      <Button
-        title="Display Stored User Info"
-        onPress={displayStoredUserInfo}
-      />
-    </SafeAreaView>
+    <View style={styles.view}>
+      <Header title={"Home"} />
+      <Calendar showEvents={true}/>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   view: {
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center',
-    padding: 40, 
+      flex: 1,
+      backgroundColor: '#2F3855',
   },
 });
 
--- calendar-app/Components/Storage/userDataStorage.js
+++ calendar-app/Components/Storage/userDataStorage.js
@@ -1,9 +1,11 @@
 import * as SecureStore from 'expo-secure-store';
 
-export async function storeUserInfo(userId, inGroup) {
+export async function storeUserInfo(userId, inGroup, isLoggedIn, groupId) {
   try {
     await SecureStore.setItemAsync('userId', userId.toString());
     await SecureStore.setItemAsync('inGroup', JSON.stringify(inGroup));
+    await SecureStore.setItemAsync('isLoggedIn', JSON.stringify(isLoggedIn));
+    await SecureStore.setItemAsync('groupId', groupId.toString());
   } catch (error) {
     console.error('Error storing the user info', error);
   }
@@ -24,5 +26,23 @@ export async function getInGroupStatus() {
     return JSON.parse(inGroup);
   } catch (error) {
     console.error('Error retrieving the inGroup status', error);
+  }
+}
+
+export async function getIsLoggedIn() {
+  try {
+    const isLoggedIn = await SecureStore.getItemAsync('isLoggedIn');
+    return JSON.parse(isLoggedIn);
+  } catch (error) {
+    console.error('Error retrieving the isLoggedIn status', error);
+  }
+}
+
+export async function getGroupId() {
+  try {
+    const groupId = await SecureStore.getItemAsync('groupId');
+    return groupId;
+  } catch (error) {
+    console.error('Error retrieving the group ID', error);
   }
 }
