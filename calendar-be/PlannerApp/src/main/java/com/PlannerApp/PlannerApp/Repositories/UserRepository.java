b2517aca805bb59e59346ade450959d9bbceb9ff
Lukrow <lukroza1@gmail.com>
2024-03-10 01:12:52 +0200

    Back end API url to get group members
    Created a backend API url to get group members and created a config file to handle postgresSQL UUID data object type

--- calendar-be/PlannerApp/src/main/java/com/PlannerApp/PlannerApp/Repositories/UserRepository.java
+++ calendar-be/PlannerApp/src/main/java/com/PlannerApp/PlannerApp/Repositories/UserRepository.java
@@ -1,12 +1,15 @@
 package com.PlannerApp.PlannerApp.Repositories;
 
 import com.PlannerApp.PlannerApp.Entities.UserEntity;
+import com.PlannerApp.PlannerApp.Models.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
+
+import java.util.List;
+import java.util.UUID;
 
 
 @Repository
@@ -16,4 +19,7 @@ public interface UserRepository {
     @Insert("INSERT INTO users (id, username) " +
             "VALUES (#{user.id}, #{user.username})")
     void insertUser(@Param("user") UserEntity user);
+
+    @Select("SELECT id, username FROM users WHERE group_id = #{groupID}")
+    List<UserEntity> getGroupUsers(@Param("groupID") UUID groupId);
 }
--- calendar-be/PlannerApp/src/main/java/com/PlannerApp/PlannerApp/Services/UserService.java
+++ calendar-be/PlannerApp/src/main/java/com/PlannerApp/PlannerApp/Services/UserService.java
@@ -1,11 +1,14 @@
 package com.PlannerApp.PlannerApp.Services;
 
 import com.PlannerApp.PlannerApp.Entities.UserEntity;
+import com.PlannerApp.PlannerApp.Models.User;
 import com.PlannerApp.PlannerApp.Repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Optional;
 
 import java.util.UUID;
@@ -25,5 +28,15 @@ public class UserService {
 
         userRepository.insertUser(userEntity);
         return userID;
+    }
+
+    public List<User> getGroupUsers(UUID groupId) {
+        return userRepository.getGroupUsers(groupId).stream()
+                .map(userEntity -> User.builder()
+                        .id(userEntity.getId())
+                        .username(userEntity.getUsername())
+                        .group_id(userEntity.getGroup_id())
+                        .build())
+                .toList();
     }
 }
