042e5a4dc7ea67f520b8161ede8c608ad91c47ed
Lukrow <lukroza1@gmail.com>
2024-03-11 14:57:11 +0200

    Added endpoint to get users by username

--- calendar-be/PlannerApp/src/main/java/com/PlannerApp/PlannerApp/Repositories/UserRepository.java
+++ calendar-be/PlannerApp/src/main/java/com/PlannerApp/PlannerApp/Repositories/UserRepository.java
@@ -16,4 +16,7 @@ public interface UserRepository {
     @Insert("INSERT INTO users (id, username) " +
             "VALUES (#{user.id}, #{user.username})")
     void insertUser(@Param("user") UserEntity user);
+
+    @Select("SELECT * FROM users WHERE username = #{username}")
+    Optional<UserEntity> getUserByUsername(@Param("username") String username);
 }
