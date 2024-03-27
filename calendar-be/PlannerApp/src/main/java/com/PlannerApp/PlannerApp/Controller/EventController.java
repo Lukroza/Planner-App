b4d4a070bc3e2cccfbc4757dabeca912a968b844
Lukrow <lukroza1@gmail.com>
2024-03-27 22:22:18 +0200

    removed comments

--- calendar-be/PlannerApp/src/main/java/com/PlannerApp/PlannerApp/Controller/EventController.java
+++ calendar-be/PlannerApp/src/main/java/com/PlannerApp/PlannerApp/Controller/EventController.java
@@ -33,7 +33,6 @@ public class EventController {
 
     @GetMapping("/getEventDetails/{eventId}")
     public EventDetails getEventDetails(@PathVariable UUID eventId){
-        System.out.println(eventService.getEventDetails(eventId));
         return eventService.getEventDetails(eventId);
     }
 }
