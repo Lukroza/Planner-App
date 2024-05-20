0eb41e50ce62b72d860ac316581e94cf84f5fb87
Lukrow <lukroza1@gmail.com>
2024-05-20 19:56:51 +0300

    Bugfix

--- calendar-app/Screens/RegistrationScreen.js
+++ calendar-app/Screens/RegistrationScreen.js
@@ -1,56 +1,68 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, Keyboard, TouchableWithoutFeedback } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import TextInputBar from '../Components/TextInputBar';
-import ButtonComp from '../Components/ButtonComp';
-import { createUserApi } from '../Components/API/Users/UserRegister';
-import { storeUserInfo } from '../Components/Storage/userDataStorage';
-import { GlobalColor } from '../Styles';
-import { loginUserAPI } from '../Components/API/Users/UsernameCheck';
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import TextInputBar from "../Components/TextInputBar";
+import ButtonComp from "../Components/ButtonComp";
+import { createUserApi } from "../Components/API/Users/UserRegister";
+import { storeUserInfo } from "../Components/Storage/userDataStorage";
+import { GlobalColor } from "../Styles";
+import { loginUserAPI } from "../Components/API/Users/UsernameCheck";
+import Toast from "react-native-toast-message";
 
 const RegistrationScreen = ({ onRefresh }) => {
-  const [username, setUsername] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [username, setUsername] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleRegistration = async () => {
     try {
-      await createUserApi({ username }).then(async (response) => {
-        await storeUserInfo(response, false, true, "0");
-        onRefresh();
-      });
+      console.log(username);
+      const response = await createUserApi({ username });
+      console.log(response);
+      await storeUserInfo(response, false, true, "0");
+      console.log("User created");
+      onRefresh();
     } catch (error) {
-      setErrorMessage(error.message);
+      console.log(error.message);
+      Toast.show({
+        type: "error",
+        text1: "Error",
+        text2: error.message,
+      });
     }
   };
 
   const handleLogin = async () => {
-    const userData = await loginUserAPI({username});
-    if(userData != null) {
-      if(userData.group_id !== null) {
+    const userData = await loginUserAPI({ username });
+    if (userData != null) {
+      if (userData.group_id !== null) {
         await storeUserInfo(userData.id, true, true, userData.group_id);
-      }
-      else{
+      } else {
         await storeUserInfo(userData.id, false, true, "0");
       }
       onRefresh();
-    } 
-    else {
+    } else {
       console.log("User not found");
     }
   };
 
   return (
-    <TouchableWithoutFeedback onPress={ () => { Keyboard.dismiss() } }>
-    <SafeAreaProvider>
-      <View style={styles.container}>
-        <TextInputBar 
-          label="Username"
-          onChangeText={setUsername}
-        />
-        <ButtonComp text="Login" onPress={handleLogin}/>
-        <ButtonComp text="Register" onPress={handleRegistration} />
-      </View>
-    </SafeAreaProvider>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <SafeAreaProvider>
+        <View style={styles.container}>
+          <TextInputBar label="Username" onChangeText={setUsername} />
+          <ButtonComp text="Login" onPress={handleLogin} />
+          <ButtonComp text="Register" onPress={handleRegistration} />
+        </View>
+      </SafeAreaProvider>
     </TouchableWithoutFeedback>
   );
 };
@@ -58,8 +70,8 @@ const RegistrationScreen = ({ onRefresh }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center', 
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: GlobalColor,
     gap: 20,
   },
